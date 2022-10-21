ad436f4b4b31407098f7e6ccc33117596027b8fe
DostovS <sohibjondostov04@gmail.com>
2022-10-21 15:37:10 +0500

    The login page is working

--- client/src/pages/LoginPage/LoginPage.js
+++ client/src/pages/LoginPage/LoginPage.js
@@ -1,11 +1,41 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import BaseCard from '../../components/UI/BaseCard/BaseCard';
 import { Link } from 'react-router-dom';
+import axios from '../../plugins/axios';
 export default function LoginPage() {
+  const inputEmail = useRef();
+  const inputPassword = useRef();
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const email = inputEmail.current.value.toLowerCase();
+    const enteredPassword = inputPassword.current.value;
+    if (email.trim().length === 0) {
+      alert("email noto'gri");
+      return false;
+    }
+    if (enteredPassword.trim().length === 0 || enteredPassword.length < 4) {
+      alert('Parol qisqa');
+      return false;
+    }
+    const loginData = {
+      email: email,
+      password: enteredPassword,
+    };
+    try {
+      const res = await axios.post("user/login", loginData);
+      localStorage.setItem("token", res.data.token);
+      localStorage.setItem("currentUser", JSON.stringify(res.data.user));
+      window.location.href = "/";
+    } catch (err) {
+      console.log(err);
+      alert("Please check your username and password");
+      return false;
+    }
+  };
   return (
     <section className="form">
           <BaseCard>
-          <form>
+          <form onSubmit={handleSubmit}>
             <h2>Login</h2>
             <p>
               Don't have an account? {" "} 
@@ -19,6 +49,8 @@ export default function LoginPage() {
               className="form-control input"
               type="text"
               placeholder="your-email@gmail.com"
+              ref={inputEmail}
+              required
             />
             </div>
             <div className="mb-3">
@@ -27,13 +59,16 @@ export default function LoginPage() {
                 className="form-control input"
                 placeholder="password123"
                 type="password"
+                required
+                ref={inputPassword}
               />
             </div>
             <div>
               <button
                 className="btn btn-success"
                 type="submit"
-                to="/">
+                to="/"
+                onClick={handleSubmit}>
                 Submit
               </button>
             </div>
