48bb1847c44d0cb13b7930127e8f9644b70340a8
Sokhibjon <98479208+DostovS@users.noreply.github.com>
2022-12-23 14:54:18 +0500

    Update axios.js

--- client/src/plugins/axios.js
+++ client/src/plugins/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "https://cw-backendd.vercel.app/api";
+axios.defaults.baseURL = "https://cw-backend-vc6k-qpv9h2eas-dostovs.vercel.app/";
 
 export default axios;
